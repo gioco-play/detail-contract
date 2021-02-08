8bfbeb2bf2f6386d5741f5e895d0666e7eb94a3c
Ellie Su <ellie.su@goldenf.co>
2021-02-08 11:48:17 +0800

    modify param

--- src/Contract/ParseDetailServiceInterface.php
+++ src/Contract/ParseDetailServiceInterface.php
@@ -8,10 +8,11 @@ interface ParseDetailServiceInterface {
     /**
      * 遊戲詳情網址產生
      *
+     * @param string $opCode 營商代碼
      * @param string $gameCode 遊戲代碼
      * @param string $betId 注單號
      * @return array
      */
-    function genDetailUrl(string $gameCode, string $betId): array;
+    function genDetailUrl(string $opCode, string $gameCode, string $betId): array;
 }
 
